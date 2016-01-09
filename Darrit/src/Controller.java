7bae009412247b2479bccd40567edc5c859c20a7
Stufingo <geniekort@hotmail.com>
2016-01-09 20:04:47 +0100

    Update

--- Darrit/src/Controller.java
+++ Darrit/src/Controller.java
@@ -1,4 +1,26 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class Controller {
+public class Controller extends JPanel{
 
+	private static final long serialVersionUID = 1L;
+
+	JFrame frame;
+	
+	public Controller(String name){
+		
+		frame = new JFrame(name);
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		new Controller("Live it");
+	}
+	
+	
+	
+	
+	
+	
 }
