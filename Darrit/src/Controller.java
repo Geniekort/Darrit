635122a83c98e40cd29e9fe423b4ab07f552566d
Stufingo <geniekort@hotmail.com>
2016-01-09 20:14:34 +0100

    Added stuff

--- Darrit/src/Controller.java
+++ Darrit/src/Controller.java
@@ -1,15 +1,27 @@
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class Controller extends JPanel{
+public class Controller extends JPanel implements ActionListener{
 
 	private static final long serialVersionUID = 1L;
 
 	JFrame frame;
+	javax.swing.Timer timer;
 	
 	public Controller(String name){
 		
 		frame = new JFrame(name);
+		frame.setSize(200, 200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+		
+		timer = new Timer(100, this);
 		
 	}
 	
@@ -18,6 +30,16 @@ public class Controller extends JPanel{
 		new Controller("Live it");
 	}
 	
+	public void paintComponent(Graphics g){
+		
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 	
