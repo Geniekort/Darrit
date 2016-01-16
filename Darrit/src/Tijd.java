30df6a761963165497bf8eacbeb265049943e13f
GerritKaasjager <gerritkaasjager@solcon.nl>
2016-01-16 20:48:42 +0100

    Dingen

--- Darrit/src/Tijd.java
+++ Darrit/src/Tijd.java
@@ -4,7 +4,33 @@ import java.awt.Graphics;
 
 public class Tijd {
 	int tijd = 0;
+	int x,y,radius;
+	int sec = 0;
 	Font font = new Font("Arial", 20, 200);
+	
+	public Tijd(int xin,int yin, int radiusin){
+		x = xin;
+		y = yin;
+		radius = radiusin;
+	}
+	
+	public void update(){
+		plus(1);
+		sec = tijd/1000;
+		
+	}
+	
+	public void setX(int setx) {
+		x = setx;
+	}
+	
+	public void setRadius(int setr){
+		radius = setr;
+	}
+	
+	public void setY(int sety){
+		y = sety;
+	}
 	
 	public void plus(int toe){
 		tijd += toe;
@@ -14,6 +40,8 @@ public class Tijd {
 		g.setColor(Color.BLACK);
 		g.setFont(font);
 		g.drawString(Integer.toString(tijd), 20,300);
+		g.drawOval(x-radius, y-radius,2*radius,2*radius);
+		g.drawLine(x, y, (Math.sin(sec/60*2*Math.PI)), Math.cos(sec/60*2*Math.PI));
 	}
 	
 	public int getTijd() {
